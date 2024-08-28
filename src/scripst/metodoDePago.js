6c840f9438622c110edac9fa00fa532dad3930ef
Gonzalez Salvador <salvadorsopas@gmail.com>
2024-08-28 00:55:34 -0600

    añadiendo los documentos MetodoDePago.html, css y js, dicha pantalla ya se encuentra con responsividad

---
+++ src/scripst/metodoDePago.js
@@ -0,0 +1,4 @@
+document.getElementById('mostrarCupon').addEventListener('click', function(event){
+    event.preventDefault();
+    document.getElementById('inputCupon').style.display= "block";
+})
