149cd92053c11a9a3187cc853e661263aab5e25b
Gonzalez Salvador <salvadorsopas@gmail.com>
2024-08-28 19:46:52 -0600

    Se agregan ambas pantallas a la pantalla detEnvMetPago.html

--- src/scripst/metodoDePago.js
+++ src/scripst/metodoDePago.js
@@ -1,4 +1,6 @@
-document.getElementById('mostrarCupon').addEventListener('click', function(event){
-    event.preventDefault();
-    document.getElementById('inputCupon').style.display= "block";
-})
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('mostrarCupon').addEventListener("click", function(event){
+        event.preventDefault();
+        document.getElementById('inputCupon').style.display = "block";
+    });
+});
